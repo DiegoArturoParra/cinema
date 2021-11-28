3216d6ca6e48e718dd0069c33aaa78f3ec9dffaa
diegoparra <48141200+DiegoArturoParra@users.noreply.github.com>
2021-11-28 14:44:12 -0500

    base

--- src/main/java/com/edu/cundi/cinema/ResourceServerConfig.java
+++
@@ -1,5 +0,0 @@
-package com.edu.cundi.cinema;
-
-public class ResourceServerConfig {
-    
-}
--- src/main/java/com/edu/cundi/cinema/services/interfaces/ILibroService.java
+++
@@ -1,7 +0,0 @@
-package com.edu.cundi.cinema.services.interfaces;
-
-import com.edu.cundi.cinema.entity.Libro;
-
-public interface ILibroService extends ICRUD<Libro, Integer> {
-
-}
